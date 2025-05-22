22ea481772a3f392daa44cba0f62f61ce5f99b0b
mersad-b7xtrue <mersadfallah.dev@gmail.com>
2025-05-22 10:38:23 +0330

    Add demo images

--- Frontend/src/services/axios.js
+++ Frontend/src/services/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = "http://usereb4064820dba043.app.vtxhub.com/api/v1";
 
 const instance = axios.create({
     baseURL: BASE_URL,
